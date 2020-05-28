2ecd066dfde8af35fa525e08384d558b9d3cf294
RAVI RANJAN <raviranjan@cse.iitb.ac.in>
2020-05-28 13:38:48 +0530

    added login and logout functionalities

--- socialmedia/socialmedia/urls.py
+++ socialmedia/socialmedia/urls.py
@@ -21,8 +21,11 @@ from django.conf.urls import include
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.HomePage.as_view(), name='home'),
-    url(r"^accounts/$", include('accounts.urls', namespace='accounts'),
-    url(r"^accounts/$", include('django.contrib.auth.urls')),
+    url(r"^accounts/", include('accounts.urls', namespace='accounts')),
+    url(r"^accounts/", include('django.contrib.auth.urls')),
+    url(r"^test/$", views.TestPage.as_view(), name="test"),
+    url(r'^thanks/$', views.ThanksPage.as_view(), name='thanks'),
+    
     
     
 ]
--- socialmedia/socialmedia/views.py
+++ socialmedia/socialmedia/views.py
@@ -1,6 +1,14 @@
 from django.views.generic import TemplateView
 
+app_name = 'accounts'
+class TestPage(TemplateView):
+    template_name = 'test.html'
+
+class ThanksPage(TemplateView):
+    template_name = 'thanks.html'
 
 class HomePage(TemplateView):
     template_name = "index.html"
+
+
 
