ceccd7953336421bbdefa6dd8776fb5c539399f6
RAVI RANJAN <raviranjan@cse.iitb.ac.in>
2020-05-29 15:04:34 +0530

    added forms.py and urls.py in the groups all folder

--- socialmedia/groups/models.py
+++ socialmedia/groups/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+# slugify helps in making normal sentences as links by adding 
+# underscores in place of spaces and making everything lowercase
+# and removing unwanted chars.
+from django.utils.text import slugify
 
+from django.core.urlresolvers import reverse
+
+# Groups models.py
 # Create your models here.
+
+# misaka is used for markdows inside of posts
+import misaka
+
+# get_user_model helps in grabbing the current user in the current user 
+# session
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+# this is to use custom template tags in future
+from django import template
+register = template.Library()
+
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group, related_name='memberships')
+    user = models.ForeignKey(User, related_name='user_group')
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        unique_together = ('group', 'user')
+    
+
+class Group(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(allow_unicode=True, unique=True)
+    description = models.TextField(blank=False, default='')
+    description_html = models.TextField(editable=True, default='', blank=True)
+    members = models.ManyToManyField(User, through='GroupMember')
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description)
+        super().save(*args, **kwargs)
+    
+    def get_absolute_url(self):
+        return reverse("groups:single", kwargs={"slug": self.slug})
+
+    class Meta:
+        ordering = 'name'    
+    
--- socialmedia/posts/models.py
+++ socialmedia/posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+# Posts models.py
+
 # Create your models here.
+
+from django.core.urlresolvers import reverse
+
+import misaka
+
+from groups.models import Group 
+
