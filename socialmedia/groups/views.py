c2dc64b01f5be8928bd30f5de695ab86581e417d
RAVI RANJAN <raviranjan@cse.iitb.ac.in>
2020-05-29 17:19:10 +0530

    completed group_list.html and group_form.html templates in the groups all

--- socialmedia/groups/views.py
+++ socialmedia/groups/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# views.py of groups app
+
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+
+from django.core.urlresolvers import reverse
+
+from . import Group, GroupMember
+
+from django.views import generic
+
+class CreateGroup(generic.CreateView):
+    fields = ('name', 'description')
+    model = Group
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+class ListGroups(generic.ListView):
+    model = Group
